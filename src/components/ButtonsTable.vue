c406c92b7b02a2766ffbbba8df8839a007f50646
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-01-29 15:01:20 +0500

    jic

--- src/components/ButtonsTable.vue
+++ src/components/ButtonsTable.vue
@@ -8,15 +8,33 @@
       </thead>
       <tbody>
         <tr v-for="item in items" :key="item.id">
-          <td v-for="column in columns" :key="column">
+          <td v-for="column in columns" :key="column" :class="{ rel: column == 'actions' }">
             <template v-if="column === 'actions'">
-              <span>
+              <span class="svg" @click="clicked[item.id] = !clicked[item.id]">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#2c3e50" viewBox="0 0 256 256">
                   <path
                     d="M128,96a32,32,0,1,0,32,32A32,32,0,0,0,128,96Zm0,48a16,16,0,1,1,16-16A16,16,0,0,1,128,144ZM48,96a32,32,0,1,0,32,32A32,32,0,0,0,48,96Zm0,48a16,16,0,1,1,16-16A16,16,0,0,1,48,144ZM208,96a32,32,0,1,0,32,32A32,32,0,0,0,208,96Zm0,48a16,16,0,1,1,16-16A16,16,0,0,1,208,144Z">
                   </path>
                 </svg>
               </span>
+              <div class="options" v-if="clicked[item.id]">
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#2c3e50" viewBox="0 0 256 256">
+                    <path
+                      d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM51.31,160,136,75.31,152.69,92,68,176.68ZM48,179.31,76.69,208H48Zm48,25.38L79.31,188,164,103.31,180.69,120Zm96-96L147.31,64l24-24L216,84.68Z">
+                    </path>
+                  </svg>
+                  <span>Edit</span>
+                </div>
+                <div>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="red" viewBox="0 0 256 256">
+                    <path
+                      d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z">
+                    </path>
+                  </svg>
+                  <span>Delete</span>
+                </div>
+              </div>
             </template>
             <template v-else>
               {{ item[column] }}
@@ -36,20 +54,32 @@ export default {
   props: ['inputValues', 'showStageButtonModal'],
   data() {
     return {
-      columns: ['id', 'btn_order', 'alias', 'is_web_app', 'back', 'created_at', 'actions'],
+      clicked: {},
+      columns: ['id', 'btn_order', 'alias', 'is_web_app', 'back', 'actions'],
       items: [],
       stage_id: this.inputValues.id ?? null,
     };
   },
   async mounted() {
     await this.getTableData();
+    this.items.forEach((item) => {
+      this.clicked[item.id] = false;
+    });
+
+    document.addEventListener('click', (e) => {
+      if (e.target.closest('.rel') == null) {
+        this.clicked = {};
+      }
+    });
 
     setInterval(() => {
       if (this.showStageButtonModal == false) {
         this.getTableData();
       }
     }, 100);
+
   },
+
   methods: {
     async getTableData() {
       if (this.stage_id) {
@@ -61,10 +91,9 @@ export default {
         if (item.created_at) {
           item.created_at = new Date(item.created_at).toLocaleDateString() + ' ' + new Date(item.created_at).toLocaleTimeString().slice(0, 5)
         }
-
-        item.actions = ``;
       }
     },
+
     addButton() {
       this.$emit('openStageButtonModal');
     },
@@ -74,8 +103,6 @@ export default {
 
 <style scoped>
 .table-container {
-  overflow-x: invisible;
-  overflow-y: hidden;
   user-select: none;
 }
 
@@ -132,7 +159,7 @@ button {
   bottom: -15px;
 }
 
-span svg {
+.svg svg {
   cursor: pointer;
   padding: 8px;
   width: 40px;
@@ -140,11 +167,60 @@ span svg {
   border-radius: 50%;
 }
 
-span svg:hover {
+.svg svg:hover {
   background-color: #ddd;
 }
 
-span svg:active {
+.svg svg:active {
+  background-color: #d4d4d4;
+}
+
+.rel {
+  position: relative;
+}
+
+/* display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+  position: absolute;
+
+  padding: 5px;
+  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
+  border-radius: 5px;
+  z-index: 10;
+  top: 40px;
+  left: 0px; 
+  background-color: white; */
+.options {
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  padding: 2px;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+  z-index: 10;
+  top: 40px;
+  left: -55px;
+  border: 1px solid #ccc;
+  border-top-right-radius: 0;
+  background-color: white;
+}
+
+.options div {
+  display: flex;
+  align-items: center;
+  gap: 0.3rem;
+  padding: 8px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+.options div:hover {
+  background-color: #f5f5f5;
+}
+
+.options div:active {
   background-color: #d4d4d4;
 }
 </style>
