73bcb8fe6e9b226fe7cb322ab4f50069e98be104
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-01-29 15:55:35 +0500

    delete stage button

--- src/components/ButtonsTable.vue
+++ src/components/ButtonsTable.vue
@@ -10,31 +10,33 @@
         <tr v-for="item in items" :key="item.id">
           <td v-for="column in columns" :key="column" :class="{ rel: column == 'actions' }">
             <template v-if="column === 'actions'">
-              <span class="svg" @click="clicked[item.id] = !clicked[item.id]">
+              <span class="svg" @click="openContextMenu(item.id)">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#2c3e50" viewBox="0 0 256 256">
                   <path
                     d="M128,96a32,32,0,1,0,32,32A32,32,0,0,0,128,96Zm0,48a16,16,0,1,1,16-16A16,16,0,0,1,128,144ZM48,96a32,32,0,1,0,32,32A32,32,0,0,0,48,96Zm0,48a16,16,0,1,1,16-16A16,16,0,0,1,48,144ZM208,96a32,32,0,1,0,32,32A32,32,0,0,0,208,96Zm0,48a16,16,0,1,1,16-16A16,16,0,0,1,208,144Z">
                   </path>
                 </svg>
               </span>
-              <div class="options" v-if="clicked[item.id]">
-                <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#2c3e50" viewBox="0 0 256 256">
-                    <path
-                      d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM51.31,160,136,75.31,152.69,92,68,176.68ZM48,179.31,76.69,208H48Zm48,25.38L79.31,188,164,103.31,180.69,120Zm96-96L147.31,64l24-24L216,84.68Z">
-                    </path>
-                  </svg>
-                  <span>Edit</span>
+              <transition name="fade">
+                <div class="options" v-if="clicked[item.id]">
+                  <div class="editButton">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#2c3e50" viewBox="0 0 256 256">
+                      <path
+                        d="M227.31,73.37,182.63,28.68a16,16,0,0,0-22.63,0L36.69,152A15.86,15.86,0,0,0,32,163.31V208a16,16,0,0,0,16,16H92.69A15.86,15.86,0,0,0,104,219.31L227.31,96a16,16,0,0,0,0-22.63ZM51.31,160,136,75.31,152.69,92,68,176.68ZM48,179.31,76.69,208H48Zm48,25.38L79.31,188,164,103.31,180.69,120Zm96-96L147.31,64l24-24L216,84.68Z">
+                      </path>
+                    </svg>
+                    <span>Edit</span>
+                  </div>
+                  <div @click="deleteButton(item.id)">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="red" viewBox="0 0 256 256">
+                      <path
+                        d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z">
+                      </path>
+                    </svg>
+                    <span>Delete</span>
+                  </div>
                 </div>
-                <div>
-                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="red" viewBox="0 0 256 256">
-                    <path
-                      d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z">
-                    </path>
-                  </svg>
-                  <span>Delete</span>
-                </div>
-              </div>
+              </transition>
             </template>
             <template v-else>
               {{ item[column] }}
@@ -81,6 +83,17 @@ export default {
   },
 
   methods: {
+    openContextMenu(itemId) {
+      if (this.clicked[itemId]) {
+        this.clicked[itemId] = false;
+      } else {
+        for (let id in this.clicked) {
+          this.clicked[id] = false;
+        }
+        this.clicked[itemId] = true;
+      }
+    },
+
     async getTableData() {
       if (this.stage_id) {
         const response = await axios.get(`http://10.20.11.24:8080/api/tg/bot/stage/button/list?stage_id=${this.stage_id}`);
@@ -92,6 +105,11 @@ export default {
           item.created_at = new Date(item.created_at).toLocaleDateString() + ' ' + new Date(item.created_at).toLocaleTimeString().slice(0, 5)
         }
       }
+    },
+
+    async deleteButton(id) {
+      await axios.delete(`http://10.20.11.24:8080/api/tg/bot/stage/button/delete?id=${id}`)
+      await this.getTableData();
     },
 
     addButton() {
@@ -145,8 +163,8 @@ button {
   align-items: center;
   justify-content: center;
   cursor: pointer;
-  width: 30px;
-  height: 30px;
+  width: 28px;
+  height: 28px;
   border-radius: 50%;
   outline: none;
   font-size: 24px;
@@ -154,9 +172,8 @@ button {
   color: white;
   border: none;
   position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: -15px;
+  right: -13px;
+  bottom: -13px;
 }
 
 .svg svg {
@@ -179,18 +196,6 @@ button {
   position: relative;
 }
 
-/* display: flex;
-  flex-direction: column;
-  gap: 0.6rem;
-  position: absolute;
-
-  padding: 5px;
-  box-shadow: 0 10px 15px -3px rgb(0 0 0 / 0.1), 0 4px 6px -4px rgb(0 0 0 / 0.1);
-  border-radius: 5px;
-  z-index: 10;
-  top: 40px;
-  left: 0px; 
-  background-color: white; */
 .options {
   display: flex;
   flex-direction: column;
@@ -222,5 +227,15 @@ button {
 
 .options div:active {
   background-color: #d4d4d4;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
