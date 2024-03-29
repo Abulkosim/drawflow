537505361eee566ba280eeef3ad29c7cb489098d
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-03-29 15:12:45 +0500

    stage selected

--- src/mixins/toggleEditorMixin.js
+++ src/mixins/toggleEditorMixin.js
@@ -1,6 +1,10 @@
 export default {
   methods: {
     toggleEditor() {
+      if (this.selected == 'URL') {
+        this.btn_type = 'INLINE'
+      }
+      
       this.editorVisible = this.selected == 'STAGE';
       if (this.editorVisible) {
         this.initializeAceEditor();
