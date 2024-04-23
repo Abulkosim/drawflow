5a5c46f797ca6a724e2d4be85e2e24d2a21c52b1
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2024-04-23 20:33:44 +0500

    fix: user id error

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -61,10 +61,9 @@ export default {
     const response = await fetchBots(this.user_id);
     const botInfo = await fetchBotInfo(this.bot_id);
 
-    if (response) {
-      if (!response.length || !botInfo || !this.bot_id || !this.user_id) {
-        this.$router.push('/error');
-      }
+
+    if (!response.length || !botInfo) {
+      this.$router.push('/error');
     }
 
     const id = document.getElementById("drawflow");
